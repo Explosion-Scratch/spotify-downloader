df15324addc4f7e9399b15a2956ac8012017da95
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 19:07:51 +0000

    Automated commit

--- runCommand.js
+++ runCommand.js
@@ -1,6 +1,6 @@
 var spawn = require("child_process").spawn;
 module.exports = (cmd, args, onData, onFinish) => {
-  var proc = spawn(cmd, args.split(" "));
+  var proc = spawn(cmd, args.match(/(?:[^\s"]+|"[^"]*")+/g));
   proc.stdout.on("data", onData);
   proc.stderr.setEncoding("utf8");
   proc.stderr.on("data", (err) => console.log(err));
