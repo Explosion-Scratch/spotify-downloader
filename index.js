3d19395b0188d95b4df32efd1810c48011e774ea
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 19:05:09 +0000

    Automated commit

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get("/downloadSong", async (req, res) => {
 
   const command = `ffmpeg -i output.mp3 -i cover.png -c:a copy -c:v copy -map 0:0 -map 1:0 -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" audio-out.mp3`;
   const run = require("./runCommand.js");
-  runCommand('ffmpeg', '-y -f concat -i list.txt -c copy output-final.mp4', 
+  run('ffmpeg', '-y -f concat -i list.txt -c copy output-final.mp4', 
         data => console.log(data), () => console.log('finished'))
 });
 
