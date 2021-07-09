2b02f2cb8dccf2146a179675149781fc4e598179
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 17:11:11 +0000

    Automated commit

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/downloadSong", async (req, res) => {
   await new Promise((resolve) => {
     ffmpeg("temp.mp3")
       .outputOptions("-c:a libmp3lame")
-      .outputOptions("-metadata", 'title="song x"')
+      .outputOptions("-metadata", 'title=')
       .save("output.mp3")
       .on("start", function (cmdline) {
         console.log("Command line: " + cmdline);
