9ada9612018cde9599832da5fa59dc4a8112c73f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 17:41:37 +0000

    Automated commit

--- index.js
+++ index.js
@@ -189,3 +189,31 @@ function toBuffer(stream) {
     stream.on("error", (err) => reject(err));
   });
 }
+const http = require('http');
+
+const download = (url, dest, cb) => {
+    const file = fs.createWriteStream(dest);
+
+    const request = http.get(url, (response) => {
+        // check if response is success
+        if (response.statusCode !== 200) {
+            return cb('Response status was ' + response.statusCode);
+        }
+
+        response.pipe(file);
+    });
+
+    // close() is async, call cb after close completes
+    file.on('finish', () => file.close(cb));
+
+    // check for request error too
+    request.on('error', (err) => {
+        fs.unlink(dest);
+        return cb(err.message);
+    });
+
+    file.on('error', (err) => { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result) 
+        return cb(err.message);
+    });
+};
