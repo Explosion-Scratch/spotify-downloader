a2418f3409e3c481ae506bc4421ae5280f74738a
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 16:44:16 +0000

    Automated commit

--- index.js
+++ index.js
@@ -61,6 +61,10 @@ app.get("/downloadSong", async (req, res) => {
     });
   });
   console.log("Promise finished");
+  ffmetadata.read("song.mp3", function (err, data) {
+    if (err) console.error("Error reading metadata", err);
+    else console.log(data);
+  });
 
   res.end();
 });
