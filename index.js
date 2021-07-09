a331d6df093d7a7b1f78583aebb07a3cfa885ff8
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 16:49:33 +0000

    Automated commit

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ app.get("/downloadSong", async (req, res) => {
   var data = {
     artist: "Me",
   };
+  
   await new Promise(resolve => setTimeout(resolve, 2000))
   meta.write("temp.mp3", data, function (err, data) {
     if (err) console.error("Error writing metadata", err);
